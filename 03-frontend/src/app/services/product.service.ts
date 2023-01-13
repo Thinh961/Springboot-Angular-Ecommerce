07276e6350c571d1936789891ea248125743dffc
ThinhNC <ncthinh.199@gmail.com>
2023-01-13 17:33:13 +0700

    thinh

--- 03-frontend/src/app/services/product.service.ts
+++ 03-frontend/src/app/services/product.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { map, Observable } from 'rxjs';
 import { Product } from '../common/product';
+import { ProductCategory } from '../common/product-category';
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +10,7 @@ import { Product } from '../common/product';
 export class ProductService {
 
   private baseUrl = 'http://localhost:8080/api/products'
+  private categoryUrl = 'http://localhost:8080/api/product-category'
 
   constructor(
     private httpClient: HttpClient
@@ -17,14 +19,26 @@ export class ProductService {
   getProductList(theCategoryId: number): Observable<Product[]> {
     const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`
 
-    return this.httpClient.get<GetResponse>(searchUrl).pipe(
+    return this.httpClient.get<GetResponseProducts>(searchUrl).pipe(
       map(response => response._embedded.products)
+    )
+  }
+
+  getProductCategories(): Observable<ProductCategory[]>{
+    return this.httpClient.get<GetResponseProductCategories>(this.categoryUrl).pipe(
+      map(response => response._embedded.productCategories)
     )
   }
 }
 
-interface GetResponse {
+interface GetResponseProducts {
   _embedded: {
     products: Product[];
+  }
+}
+
+interface GetResponseProductCategories {
+  _embedded: {
+    productCategories: ProductCategory[];
   }
 }
